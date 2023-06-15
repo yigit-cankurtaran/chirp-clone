80f9f437c17899b2a9ff6bbd7f1cc6be12a30b01
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-06-15 09:48:40 +0300

    Added rate limiter.

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,8 @@ import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
@@ -15,6 +17,13 @@ const filterUserForClient = (user: User) => {
     profileImageUrl: user.profileImageUrl,
   }
 }
+
+// Create a ratelimiter that allows 3 requests every 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+})
 
 
 
@@ -50,6 +59,7 @@ export const postsRouter = createTRPCRouter({
     content: z.string().emoji().min(1).max(280),
   })).mutation(async ({ctx, input}) => {
     const authorId = ctx.userId;
+    const {success} = await ratelimit.limit(authorId);
     const post = await ctx.prisma.post.create({
       data: {
         authorId,
